ca4a5f367d79651ecbd4adc36f6b9663fd350feb
Sherlock Kuo <nephoooooo@gmail.com>
2021-03-08 23:50:34 +0800

    Updates

---
+++ precache-manifest.808392298144b73c371e40550757ea79.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41abf4e9bdda1159868634985bfbce0f",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.2f07b3b66e2e8fa72201.bundle.js"
+  },
+  {
+    "url": "runtime~main.2f07b3b66e2e8fa72201.bundle.js"
+  },
+  {
+    "url": "vendors~main.2f07b3b66e2e8fa72201.bundle.js"
+  },
+  {
+    "url": "vendors~main.2f07b3b66e2e8fa72201.bundle.js.LICENSE.txt"
+  }
+]);
